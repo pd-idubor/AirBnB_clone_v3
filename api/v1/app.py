b2bbe2224ea6e3636cdccf67e96ac3a037cc5325
Precious Idubor <preciousidubor@gmail.com>
2023-02-16 12:26:14 -0800

    Doc

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+    Methods
+"""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+    Routing
+"""
 from api.v1.views import app_views
 from flask import jsonify
 
